61bbadb043b8a16feed1438032d71580d8c5ee4d
mbouaklaine <98900610+mbouaklaine@users.noreply.github.com>
2022-02-03 13:22:15 -0500

    Update Agentlinux.sh

--- Agentlinux.sh
+++ Agentlinux.sh
@@ -1,19 +1,22 @@
-$URL="https://dev.azure.com/allymeer-hossen/"
-$2="PAT"
-$3="khjp73bizufgmvzzm6mh3puieqxztvbpfwkyqy4our5mzzavdw6a"
-$4="testlinux"
+#!/bin/bash
+#pool=${testlinux}
+#pat=${khjp73bizufgmvzzm6mh3puieqxztvbpfwkyqy4our5mzzavdw6a}
+#azdourl=${https://dev.azure.com/allymeer-hossen/}
+
+# install az cli
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+
 #create installation directory
 sudo mkdir $(pwd)/myagent
 sudo chmod -R o+w $(pwd)/myagent
 cd $(pwd)/myagent
-
 #Download and install the agent
 sudo wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz
 sudo tar zxvf *.tar.gz
 cd ..
 sudo chmod -R o+w $(pwd)/myagent
 cd $(pwd)/myagent
-./config.sh --unattended --url "$URL"  --auth PAT  --token "$PAT" --pool "$POOL" --agent $(hostname) --runAsService
+./config.sh --unattended --url $1 --auth pat --token $2 --pool $3 --agent $(hostname) --runAsService
 sudo ./svc.sh install 
 sudo ./svc.sh start 
 sudo ./svc.sh status
