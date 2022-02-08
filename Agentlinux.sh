79dafb0e03bc7819986ff61655a284761b414e62
mbouaklaine <98900610+mbouaklaine@users.noreply.github.com>
2022-02-08 09:15:41 -0500

    Update Agentlinux.sh

--- Agentlinux.sh
+++ Agentlinux.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-#pool=${testlinux}
-#pat=${khjp73bizufgmvzzm6mh3puieqxztvbpfwkyqy4our5mzzavdw6a}
-#azdourl=${https://dev.azure.com/allymeer-hossen/}
+#$agentuser=${azureuser}
+pool="testlinux"
+pat="srkne4chzomco2eejelnlsu66er55qpxiyftjb6odnlurvxj554a"
+azdourl="https://dev.azure.com/allymeer-hossen/"
 
 # install az cli
-curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+wget https://aka.ms/InstallAzureCLIDeb | sudo bash
 
-#create installation directory
+# download azdo agent
 sudo mkdir $(pwd)/myagent
-sudo chmod -R o+w $(pwd)/myagent
 cd $(pwd)/myagent
-#Download and install the agent
-sudo wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz
-sudo tar zxvf *.tar.gz
+wget https://vstsagentpackage.azureedge.net/agent/2.196.2/vsts-agent-linux-x64-2.196.2.tar.gz  # Newer versions may be available at the time you're reading this
+tar xzvf *.tar.gz
+
+# configure as azdouser
 cd ..
 sudo chmod -R o+w $(pwd)/myagent
 cd $(pwd)/myagent
-./config.sh --unattended --url "https://dev.azure.com/allymeer-hossen/" --auth pat --token "srkne4chzomco2eejelnlsu66er55qpxiyftjb6odnlurvxj554a" --pool "testlinux" --agent $(hostname) --runAsService
+./config.sh --unattended --url $3 --auth pat --token $2 --pool $1 --agent $(hostname) --runAsService
+
+# install and start the service
 sudo ./svc.sh install 
 sudo ./svc.sh start 
 sudo ./svc.sh status
