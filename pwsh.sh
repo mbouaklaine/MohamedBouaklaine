89e95c4b64da4ef23b4c44f975a793f2c555e4a4
mbouaklaine <98900610+mbouaklaine@users.noreply.github.com>
2022-02-14 10:16:51 -0500

    Update pwsh.sh

--- pwsh.sh
+++ pwsh.sh
@@ -10,15 +10,16 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 #echo "$1" > /tmp/echofile
 #echo "$2" > /tmp/echofile2
 #echo "$3" > /tmp/echofile3
-sudo mkdir /myagent
-cd /myagent
+cd /home/azueruser
+sudo mkdir myagent
+cd myagent
 sudo wget https://vstsagentpackage.azureedge.net/agent/2.186.1/vsts-agent-linux-x64-2.186.1.tar.gz
 sudo tar zxvf ./vsts-agent-linux-x64-2.186.1.tar.gz
 sudo chmod -R 777 /myagent
 runuser -l azureuser -c "/myagent/config.sh --unattended  --url $1 --auth pat --token $2 --pool $3"
 #/myagent/config.sh --unattended  --url "$1" --auth pat --token "$2" --pool "$3"
-sudo /myagent/svc.sh install
-sudo /myagent/svc.sh start
+sudo /home/azueruser/myagent/svc.sh install
+sudo /home/azueruser/myagent/svc.sh start
 #exit 0
 #Install Azure CLI
 curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
