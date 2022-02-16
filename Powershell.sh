24046ad212aefb8ebb6a10dae1372f71d3cac9ab
mbouaklaine <98900610+mbouaklaine@users.noreply.github.com>
2022-02-16 16:49:35 -0500

    Update Powershell.sh

--- Powershell.sh
+++ Powershell.sh
@@ -1,3 +1,3 @@
-pwsh -c "Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force"
-pwsh -c "Install-Module Az -AllowClobber -Force"
+pwsh -c "Install-Module -Name Az -AllowClobber -Force"
+
 pwsh -c "Update-Module -Name Az -Force"
